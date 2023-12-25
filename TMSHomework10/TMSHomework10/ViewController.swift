ae803c6ca2aeedd73ed1e8925bedf7dbf807b46e
Mir TImur <mirtimur1@gmail.com>
2023-12-25 13:55:39 +0300

    Initial commit

---
+++ TMSHomework10/TMSHomework10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMSHomework10
+//
+//  Created by Timur Mir on 25.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
